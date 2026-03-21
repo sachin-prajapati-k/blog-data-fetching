7dc0da5117f926ef716edadb7b87a6906c204eec
sagar <sdev77577@gmail.com>
2026-03-21 21:23:46 +0530

    asdf

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -12,6 +12,23 @@ export async function GET() {
 
 
 export async function POST(request:Request) {
-    
+    const body=await request.json();
+    try{
+        if(!body.name || !body.email){
+            return NextResponse.json({error:'email and name are required'},{status:400})
+        }
+        const existingUser=Users.find((u)=>u.email===body.email);
+        if(existingUser){
+            return NextResponse.json({error:'user already registered with this email'},{status:409})
+        }
+        const newUser={
+            id:Math.max(...Users.map(u=>u.id))+1,
+            name:body.name,
+            email:body.email
+        }
+        Users.push(newUser);
+        return NextResponse.json(newUser,{status:201})
+    }
+
     
 }
