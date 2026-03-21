478a6827d39e3ab74fb04098709c90091b153026
sagar <sdev77577@gmail.com>
2026-03-21 21:58:27 +0530

    sf

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -33,24 +33,53 @@ export async function GET(request: Request, { params }: IParams) {
   return NextResponse.json(userWithDetails, { status: 201 });
 }
 
-
-export async function PUT(request:Request,{params}:IParams) {
-    await new Promise(resolve=>setTimeout(resolve,1000));
-    const {id}=await params;
-    const userId=parseInt(id);
-    try{if(isNaN(userId)){
-        return NextResponse.json({error:'user id is not valid'},{status:404})
+export async function PUT(request: Request, { params }: IParams) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const { id } = await params;
+  const userId = parseInt(id);
+  try {
+    if (isNaN(userId)) {
+      return NextResponse.json(
+        { error: "user id is not valid" },
+        { status: 404 },
+      );
     }
-const userIndex=Users.findIndex(u=>u.id===userId);
-if(userIndex===-1){
-    return NextResponse.json({error:'user not found'},{status:404})
-}
-const body=await request.json();
-const updateUser={
-    ...Users[userIndex],
-    
-}
+    const userIndex = Users.findIndex((u) => u.id === userId);
+    if (userIndex === -1) {
+      return NextResponse.json({ error: "user not found" }, { status: 404 });
+    }
+    const body = await request.json();
+    const updateUser = {
+      ...Users[userIndex],
+      ...body,
+      id: userId,
+    };
+    Users[userIndex] = updateUser;
+    return NextResponse.json(updateUser);
+  } catch (Error) {
+    return NextResponse.json(
+      { Error: "id invalid please try agin" },
+      { status: 404 },
+    );
+  }
 }
 
-    
+export async function Delete(request: Request, { params }: IParams) {
+  const { id } = await params;
+  const userId = parseInt(id);
+  if (isNaN(userId)) {
+    return NextResponse.json({ error: "user id invalid" }, { status: 404 });
+  }
+  const userIndex = Users.findIndex((u) => u.id === userId);
+  if (userIndex === -1) {
+    return NextResponse.json(
+      { error: "user does not exists with this id " },
+      { status: 409 },
+    );
+  }
+  const deletedUser = Users.splice(userIndex, 1)[0];
+  return NextResponse.json(
+    { message: "user deleted successfully", user: deletedUser },
+    { status: 201 },
+  );
 }
