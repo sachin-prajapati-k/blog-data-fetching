5f5a1cd99e751b9d131f88e2bedf1384c50c1916
sagar <sdev77577@gmail.com>
2026-03-22 15:29:44 +0530

    dsfg

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,11 +1,10 @@
-
 import { Users, Posts } from "@/src/data/mockData";
 import { NextResponse } from "next/server";
 export async function GET() {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   const userWithPost = Users.map((u) => ({
     ...u,
-    post: Posts.find((p) => p.authorId === u.id),
+    posts: Posts.filter((p) => p.authorId === u.id),
   }));
   return NextResponse.json(userWithPost);
 }
